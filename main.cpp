1bd71b5e2671e7afa6f9d6740890d92fc2789263
J24RM <rodriguezmendozajesus8@gmail.com>
2024-12-01 13:26:46 -0600

    Update main.cpp
    Se agregaron comentarios que explican el funcionamiento del codigo

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+//Creacion 9 nov 2024
+//Modificación 29 nov 2024
+//Autor: Jesús Rodriguez Mendoza A01713550
+
+//Descripción: Main: se maneja toda la logica de los combates y el codigo 
+
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -5,16 +12,17 @@ using namespace std;
 #include "Goblin.h"
 #include "Mago.h"
 
-
+// El jugador ataca a un enemigo quitandole vida, el parametro de entrada es al enemigo que va a atacar
 void AtaqueDePlayer(Player& player, Enemigo& enemy) {
     int VidaNueva = enemy.get_Vida() - player.get_Fuerza();
     enemy.set_Vida(VidaNueva);
+    cout << "Atacaste al " << enemy.get_Nombre() << " ,Vida del " << enemy.get_Nombre() << ":" << enemy.get_Vida() << endl;
 }
 
 int main() {
 
-    int x;
-    int y;
+    int comprobarSiEsaOpcionExiste;
+    int playerSiguevivo;
     int opc;
     int objetivo;
 
@@ -26,23 +34,25 @@ int main() {
     player.set_Nombre(nombreJugador);
 
     // Crear enemigos
-    Goblin goblin1(3, "Goblin Juan", 5);
-    Goblin goblin2(4, "Goblin Pepe", 10);
+    Goblin goblin1(1, "Goblin Juan", 5);
+    Goblin goblin2(1, "Goblin Pepe", 10);
     Mago mago1(10, "Mago Lazaro", 10);
-    Mago mago2(8, "Mago KiKo", 5);
+    Mago mago2(10, "Mago KiKo", 5);
 
-    CampoDeBatalla campo("tormenta",player,goblin1,goblin2,mago1,mago2);
+    CampoDeBatalla campo = CampoDeBatalla("tormenta");
 
 
     // Ciclo principal
-    y = 0;
-    cout << "Hay tormenta electrica cada 2 turnos le cae un rayo a cada peleador" << endl;
-    while (y == 0 && (goblin1.get_Vida() > 0 || goblin2.get_Vida() > 0 || mago1.get_Vida() > 0 || mago2.get_Vida() > 0)) {
+    playerSiguevivo = 0;
+    while (playerSiguevivo == 0 && (goblin1.get_Vida() > 0 || goblin2.get_Vida() > 0 || mago1.get_Vida() > 0 || mago2.get_Vida() > 0)) {
+        //Efectos de campo
         campo.incrementarTurno();
-        campo.Tormenta(); // Aplicar efecto de tormenta en cada turno
-        x = 0;
-        while (x == 0) {
-            cout << "Vida del Jugador:" << player.get_vida() << endl;
+        campo.Tormenta(player, goblin1, goblin2, mago1, mago2);
+
+        comprobarSiEsaOpcionExiste= 0;
+        //Player menu para atacar
+        while (comprobarSiEsaOpcionExiste == 0) {
+            cout << "Vida del Jugador:" << player.get_Vida() << endl;
             cout << "1.- Atacar" << endl;
             cin >> opc;
 
@@ -64,27 +74,24 @@ int main() {
 
             if (objetivo == 1 && goblin1.get_Vida() > 0) {
                 AtaqueDePlayer(player ,goblin1);
-                x = 1;
+                comprobarSiEsaOpcionExiste = 1;
             } 
             else if (objetivo == 2 && goblin2.get_Vida() > 0) {
                 AtaqueDePlayer(player ,goblin2);
-                x = 1;
+                comprobarSiEsaOpcionExiste = 1;
             } 
             else if (objetivo == 3 && mago1.get_Vida() > 0) {
                 AtaqueDePlayer(player ,mago1);
-                x = 1;
+                comprobarSiEsaOpcionExiste = 1;
             } 
             else if (objetivo == 4 && mago2.get_Vida() > 0) {
                 AtaqueDePlayer(player ,mago2);
-                x = 1;
+                comprobarSiEsaOpcionExiste = 1;
             } 
             else {
                 cout << "No existe esa opción" << endl;
             }
             }         
-            else {
-                cout << "No existe esa opción" << endl;
-            }
         }
  
             // Ataques enemigos
@@ -113,10 +120,11 @@ int main() {
                     mago2.regenerar_Magia();
                 }
             }
-            if (player.get_vida() < 0){
-                y = 1;
+            if (player.get_Vida() < 0){
+                playerSiguevivo = 1;
                 cout << "Moriste" << endl;
             }
+
     }
 
     return 0;
