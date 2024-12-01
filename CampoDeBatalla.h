13c90064e525c2afd9fc12b7e930bb57ef83342b
J24RM <rodriguezmendozajesus8@gmail.com>
2024-12-01 13:22:17 -0600

    Update CampoDeBatalla.h
    Se agregaron comentarios para un mejor entendimiento del codigo, se cambiaron los nombres de los metodos para que cumpla con los estandares y la composición ahora se realiza con los hijos de la clase Enemigo.

--- CampoDeBatalla.h
+++ CampoDeBatalla.h
@@ -1,45 +1,52 @@
+//Creacion 9 nov 2024
+//Modificación 29 nov 2024
+//Autor: Jesús Rodriguez Mendoza A01713550
+
+//Descripción: Clase campo de batalla, se encarga de manejar todos los estados de campo, como una tormenta electrica
+
 #ifndef CAMPODEBATALLA_H
 #define CAMPODEBATALLA_H
 
 #include "Player.h"
 #include "Enemigo.h"
+#include "Goblin.h"
+#include "Mago.h"
 
 class CampoDeBatalla {
 private:
     string clima;
     int turnoActual;
     Player player;
-    Enemigo enemy1;
-    Enemigo enemy2;
-    Enemigo enemy3;
-    Enemigo enemy4;
+    Goblin enemy1;
+    Goblin enemy2;
+    Mago enemy3;
+    Mago enemy4;
 
 public:
-    CampoDeBatalla(string _clima, Player _player, Enemigo _enemy1,Enemigo _enemy2, Enemigo _enemy3, Enemigo _enemy4){
+    //Constructor
+    CampoDeBatalla(string _clima){
         clima = _clima;
-        player = _player;
-        enemy1 = _enemy1;
-        enemy2 = _enemy2;
-        enemy3 = _enemy3;
-        enemy4 = _enemy4;
 
     }
 
-    void incrementarTurno() {
+    //Funcion que incrementa los turnos, lo hace como si fuera paso de tiempo
+    void IncrementarTurno() {
         turnoActual++;
     }
 
-    // Efecto de tormenta cada 2 turnos
-    void Tormenta() {
+    // Realiza un ataca que quita 5 de vida a cada jugador, el rayo cae cada dos turnos
+    void Tormenta(Player& player, Goblin& enemy1, Goblin& enemy2, Mago& enemy3, Mago& enemy4) {
         if (clima == "tormenta" && turnoActual % 2 == 0) {
             cout << "¡Un rayo cae sobre todos los personajes! Todos pierden 5 puntos de vida." << endl;
-            player.set_vida(player.get_vida() - 5);
-            if (enemy1.get_Vida() > 0) enemy1.set_Vida(enemy1.get_Vida() - 5);
-            if (enemy2.get_Vida() > 0) enemy2.set_Vida(enemy2.get_Vida() - 5);
-            if (enemy3.get_Vida() > 0) enemy3.set_Vida(enemy3.get_Vida() - 5);
-            if (enemy4.get_Vida() > 0) enemy4.set_Vida(enemy4.get_Vida() - 5);
+            player.set_Vida(player.get_Vida() - 5);
+            enemy1.set_Vida(enemy1.get_Vida() - 5);
+            enemy2.set_Vida(enemy2.get_Vida() - 5);
+            enemy3.set_Vida(enemy3.get_Vida() - 5);
+            enemy4.set_Vida(enemy4.get_Vida() - 5);
         }
+
     }
+
 };
 
 #endif
