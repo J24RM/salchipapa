6a1560787624010fefc03b62406e1317ea9d46fa
J24RM <rodriguezmendozajesus8@gmail.com>
2024-12-01 13:25:33 -0600

    Update Mago.h
    Se agregaron comentarios para explicar que hace cada metodo y una descripción del codigo y se cambiaron el nombre de los metodos para cumplir los estandares.

--- Mago.h
+++ Mago.h
@@ -1,3 +1,9 @@
+//Creación 23 nov 2024 
+//Modificación 29 nov 2024
+//Autor: Jesús Rodriguez Mendoza A01713550
+
+//Descripción Clase Hija de Enemigo, El Mago es un tipo de enemgio que ataca con magia, hace más daño que el goblin pero tiene un limite de magia
+
 #ifndef MAGO_H
 #define MAGO_H
 #include <iostream>
@@ -7,35 +13,39 @@ using namespace std;
 
 class Mago: public Enemigo{
 	private:
-		int Magia;
+		int magia;
 
 	public:
+		//Constructores 
 		Mago():Enemigo(){
-			Magia = 0;
+			magia = 0;
 		}
 
-		Mago(int _Magia,string _Nombre,int _Vida): Enemigo(_Nombre,_Vida){
-			Magia = _Magia;
+		Mago(int _magia,string _nombre,int _vida): Enemigo(_nombre,_vida){
+			magia = _magia;
 		}
-
+		//Getter
 		int get_Magia(){
-			return Magia;
+			return magia;
 		}
 
+		//Metodos
+
+		//Sirve para atacar al enemigo con hechizo, el mago al atacar se le baja 3 de magia, parametro de entrada es al objetivo al que va a atacar
 		void Hechizo(Player& player)
 		{
-			//ACOMODAR
-			int VidaNueva = player.get_vida() - 5;
-        	Magia -= 3; // Reduce 3 de maná
-       		player.set_vida(VidaNueva);
-       		cout << Nombre << " lanzó un hechizo contra " << player.get_Nombre() << " causando 5 de daño!" << endl;
+			int vida_Nueva = player.get_Vida() - 5;
+        	magia -= 3; // Reduce 3 de magia
+       		player.set_Vida(vida_Nueva);
+       		cout << nombre << " lanzó un hechizo contra " << player.get_Nombre() << " causando 5 de daño!" << endl;
     			
 		}
 
+		//Regenera la magia hasta en un maximo de 20.
 		void regenerar_Magia(){
 			// El mago regenera 5 de mana de 20 
-			Magia = min(20, Magia + 5); // Regenera hasta un máximo de 20
-    		cout << Nombre << " regeneró 5 puntos de magia. Magia actual: " << Magia << endl;
+			magia = min(20, magia + 5); // Regenera hasta un máximo de 20
+    		cout << nombre << " regeneró 5 puntos de magia. Magia actual: " << magia << endl;
 		}
 
 };
