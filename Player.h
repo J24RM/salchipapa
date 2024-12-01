3d827622536374782b4aab933517f6120570ed30
J24RM <rodriguezmendozajesus8@gmail.com>
2024-12-01 13:26:09 -0600

    Update Player.h
    Se agregaron comentarios y se cambiaron el nombre de los metodos para cumplir los estandares de programación

--- Player.h
+++ Player.h
@@ -1,3 +1,10 @@
+//Creacion 9 nov 2024
+//Modificación 29 nov 2024
+//Autor: Jesús Rodriguez Mendoza A01713550
+
+//Descripción: Clase del jugador, en esta clase se maneja todo lo que tenga que ver con el jugador
+//el ataque del jugador se hace desde el main por que si llamo a la clase enemigo aqui se crea una dependencia circular
+
 #ifndef PLAYER_H
 #define PLAYER_H
 #include <iostream>
@@ -6,41 +13,43 @@ using namespace std;
 
 class Player {
     private:
-        string Nombre;
-        int Fuerza;
-        int Vida;
+        string nombre;
+        int fuerza;
+        int vida;
 
     public:
+        //Constructores
         Player() {
-            Nombre = "Link";
-            Fuerza = 10;
-            Vida = 1;
+            nombre = "Link";
+            fuerza = 5;
+            vida = 50;
 
         }
 
         //Setters
-        void set_Nombre(string _Nombre) {
-            Nombre = _Nombre;
+        void set_Nombre(string _vombre) {
+            nombre = _vombre;
         }
 
-        void set_vida(int _Vida) {
-            Vida = _Vida;
+        void set_Vida(int _vida) {
+            vida = _vida;
         }
 
         //Getters
 
          string get_Nombre(){
-            return Nombre;
+            return nombre;
          }
 
-        int get_vida() {
-            return Vida;
+        int get_Vida() {
+            return vida;
         }
 
         int get_Fuerza(){
-            return Fuerza;
+            return fuerza;
         } 
     
 };
 
 #endif
+
